d8f563865d4bbd8c393ad15e8eb872de50dd1aaa
maria.martinez_gofun <maria.martinez@gofun.com.es>
2024-10-07 13:17:51 +0200

    add toast

--- app/src/main/java/com/gofun/landi_service/DeviceDataActivity.kt
+++ app/src/main/java/com/gofun/landi_service/DeviceDataActivity.kt
@@ -3,6 +3,7 @@ package com.gofun.landi_service
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.sdksuite.corebridge.api.DeviceTag
 
@@ -10,6 +11,7 @@ class DeviceDataActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val resultIntent = Intent()
+        Toast.makeText(this, "GOFUN-LANDI service", Toast.LENGTH_SHORT).show()
         try {
             val deviceTag = intent.getStringExtra("DEVICE_TAG")!!
             val value = LandiApplication.getCoreBridge().getSystemAbility().getDeviceInfo(DeviceTag.valueOf(deviceTag))
